6ef6639f787aab69ee70e0dec95d9e3be1abd160
Ali Muzaffar <ali@muzaffar.me>
2016-10-15 12:35:00 +1100

    Initial commit showing a frosted header and a frosted card view type layout

---
+++ app/src/main/java/com/alimuzaffar/demo/frosty/CardViewActivity.java
@@ -0,0 +1,166 @@
+package com.alimuzaffar.demo.frosty;
+
+import android.databinding.DataBindingUtil;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.LightingColorFilter;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v8.renderscript.RenderScript;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+
+import com.alimuzaffar.demo.frosty.databinding.ActivityCardViewBinding;
+
+public class CardViewActivity extends AppCompatActivity {
+    ActivityCardViewBinding mBinding;
+    RenderScript rs;
+    //Our background will not scroll
+    //so cache the blurred bitmap
+    Bitmap mBlurredBitmap = null;
+    Bitmap mBitmap1 = null;
+    Bitmap mBitmap2 = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_card_view);
+
+        mBinding.scrollView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v,
+                                       int left, int top,
+                                       int right, int bottom,
+                                       int oldLeft, int oldTop,
+                                       int oldRight, int oldBottom) {
+                mBitmap1 = loadBitmap(mBinding.imgBg, mBinding.cardview, 0);
+                mBinding.cardview.setBackground(new BitmapDrawable(getResources(), mBitmap1));
+                mBitmap2 = loadBitmap(mBinding.imgBg, mBinding.cardview2, 0);
+                mBinding.cardview2.setBackground(new BitmapDrawable(getResources(), mBitmap2));
+            }
+        });
+
+        mBinding.scrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged() {
+                int scrollY = mBinding.scrollView.getScrollY(); // For ScrollView
+                if (mBitmap1 != null) {
+                    mBitmap1.recycle();
+                }
+                if (mBitmap2 != null) {
+                    mBitmap2.recycle();
+                }
+                mBitmap1 = loadBitmap(mBinding.imgBg, mBinding.cardview, scrollY);
+                mBinding.cardview.setBackground(new BitmapDrawable(getResources(), mBitmap1));
+                mBitmap2 = loadBitmap(mBinding.imgBg, mBinding.cardview2, scrollY);
+                mBinding.cardview2.setBackground(new BitmapDrawable(getResources(), mBitmap2));
+            }
+        });
+        rs = RenderScript.create(this);
+    }
+
+    private Bitmap loadBitmap(View backgroundView, View targetView, int scrollY) {
+        Rect backgroundBounds = new Rect();
+        backgroundView.getHitRect(backgroundBounds);
+        if (targetView.getLocalVisibleRect(backgroundBounds)) {
+            // Any portion of the imageView, even a single pixel, is within the visible window
+            //Log.d("TAG", "VISIBLE");
+        } else {
+            // NONE of the imageView is within the visible window
+            Log.d("TAG", "NOT VISIBLE VISIBLE");
+            return null;
+        }
+        Bitmap blurredBitamp = captureView(backgroundView);
+        Matrix matrix = new Matrix();
+        //half the size of the cropped bitmap
+        //to increase performance, it will also
+        //increase the blur effect.
+        matrix.setScale(0.5f, 0.5f);
+        //capture only the area covered by our target view
+        int [] loc = new int[2];
+        int [] bgLoc = new int[2];
+        backgroundView.getLocationInWindow(bgLoc);
+        targetView.getLocationInWindow(loc);
+        int height = targetView.getHeight();
+        int y = loc[1];
+        if (bgLoc[1] >= loc[1]) {
+            //view is going off the screen at the top
+            height -= (bgLoc[1] - loc[1]);
+            if (y < 0)
+                y = 0;
+        }
+        Log.d("TAG", "Y = " + y + ", blurBitmapHeight = "+blurredBitamp.getHeight());
+        if (y + height > blurredBitamp.getHeight()) {
+            height = blurredBitamp.getHeight() - y;
+            Log.d("TAG", "Height = " + height);
+            if (height <= 0) {
+                //below the screen
+                return null;
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(blurredBitamp,
+                (int) targetView.getX(),
+                y,
+                targetView.getMeasuredWidth(),
+                height,
+                matrix,
+                true);
+
+
+
+        //Create rounded corners on the Bitmap
+        //keep in mind that our bitmap is half
+        //the size of the original view, setting
+        //it as the background will stretch it out
+        //so you will need to use a smaller value
+        //for the rounded corners than you would normally
+        //to achieve the correct look.
+        return ImageHelper.roundCorners(
+                bitmap,
+                getResources().getDimensionPixelOffset(R.dimen.rounded_corner),
+                false);
+    }
+
+    public Bitmap captureView(View view) {
+        if (mBlurredBitmap != null) {
+            return mBlurredBitmap;
+        }
+        //Find the view we are after
+        //Create a Bitmap with the same dimensions
+        mBlurredBitmap = Bitmap.createBitmap(view.getMeasuredWidth(),
+                view.getMeasuredHeight(),
+                Bitmap.Config.ARGB_4444); //reduce quality and remove opacity
+        //Draw the view inside the Bitmap
+        Canvas canvas = new Canvas(mBlurredBitmap);
+        view.draw(canvas);
+
+        //blur it
+        ImageHelper.blurBitmapWithRenderscript(rs, mBlurredBitmap);
+
+        //Make it frosty
+        Paint paint = new Paint();
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        ColorFilter filter = new LightingColorFilter(0xFFFFFFFF, 0x00222222); // lighten
+        //ColorFilter filter = new LightingColorFilter(0xFF7F7F7F, 0x00000000);    // darken
+        paint.setColorFilter(filter);
+        canvas.drawBitmap(mBlurredBitmap, 0, 0, paint);
+
+        return mBlurredBitmap;
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mBlurredBitmap != null) {
+            mBlurredBitmap.recycle();
+        }
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/alimuzaffar/demo/frosty/ScrollingActivity.java
@@ -0,0 +1,148 @@
+package com.alimuzaffar.demo.frosty;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.LightingColorFilter;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.v8.renderscript.RenderScript;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+public class ScrollingActivity extends AppCompatActivity {
+    private RenderScript rs = null;
+    ImageView imgBgBlur;
+    View imgBg;
+    Bitmap mBlurBitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        imgBgBlur = (ImageView) findViewById(R.id.imgBgBlur);
+        imgBg = findViewById(R.id.container);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgBgBlur.setTop(imgBgBlur.getTop() - 10);
+            }
+        });
+
+        final NestedScrollView nestedScrollView =
+                (NestedScrollView) findViewById(R.id.nestedScrollView);
+        final AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.app_bar);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange()) {
+                    Log.d("TAG", "Collapsed");
+                    // Collapsed
+                } else {
+                    //Log.d("TAG", "Top for BlurImg" + imgBgBlur.getTop());
+                    imgBgBlur.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        nestedScrollView.setOnScrollChangeListener(
+                new NestedScrollView.OnScrollChangeListener() {
+
+                    @Override
+                    public void onScrollChange(NestedScrollView v,
+                                               int scrollX, int scrollY,
+                                               int oldScrollX, int oldScrollY) {
+                        imgBgBlur.setTop(v.getTop() - scrollY);
+                        imgBgBlur.setVisibility(View.VISIBLE);
+                    }
+                });
+
+        nestedScrollView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom,
+                                       int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                if (mBlurBitmap == null) {
+                    mBlurBitmap = createBlurBitmap();
+                    imgBgBlur.setImageBitmap(mBlurBitmap);
+                }
+            }
+        });
+        rs = RenderScript.create(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public Bitmap captureView(View view) {
+        //Find the view we are after
+        //Create a Bitmap with the same dimensions
+        Bitmap image = Bitmap.createBitmap(view.getMeasuredWidth(),
+                view.getMeasuredHeight(),
+                Bitmap.Config.ARGB_4444); //reduce quality and remove opacity
+        //Draw the view inside the Bitmap
+        Canvas canvas = new Canvas(image);
+        view.draw(canvas);
+
+        //Make it frosty
+        Paint paint = new Paint();
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        ColorFilter filter = new LightingColorFilter(0xFFFFFFFF, 0x00222222); // lighten
+        //ColorFilter filter = new LightingColorFilter(0xFF7F7F7F, 0x00000000);    // darken
+        paint.setColorFilter(filter);
+        canvas.drawBitmap(image, 0, 0, paint);
+
+        return image;
+    }
+
+    public Bitmap createBlurBitmap() {
+        Bitmap bitmap = captureView(imgBg);
+        if (bitmap != null) {
+            ImageHelper.blurBitmapWithRenderscript(rs, bitmap);
+        }
+
+        return bitmap;
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mBlurBitmap != null) {
+            mBlurBitmap.recycle();
+        }
+        super.onDestroy();
+    }
+
+}
